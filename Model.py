5f3dba129f847ff4778149aaeaa0a6d2ab02e915
kieron2411 <59542777+kieron2411@users.noreply.github.com>
2022-12-02 12:37:24 +0000

    Update Model.py
    Randomised starting position of infected agents.

--- Model.py
+++ Model.py
@@ -1,4 +1,8 @@
 import mesa     #imports the "mesa" module. This module is used for Agent Based Modelling
+import random
+
+#inspiration taken from the below GitHub repository
+#https://github.com/projectmesa/mesa/tree/main/examples/schelling
 
 class DiseaseAgent(mesa.Agent): #defines a class called "DiseaseAgent" that inherits from the Agent class from the mesa module
 
@@ -16,20 +20,20 @@ class DiseaseAgent(mesa.Agent): #defines a class called "DiseaseAgent" that inhe
                     
             for i in range(infected_neighbors): #for every infected neighbor it will run this block
                 if self.random.random() <= self.model.beta: #generates a random number and checks if it is less than "beta" (chance of being infected)
-                    self.type = 3   #sets the agent to be a temporary type which will be changed to infected
+                    self.type = 3   #sets the agent to be infected
                     self.model.susceptible -= 1     #removes 1 from the count of susceptible agents
                     self.model.infected += 1    #adds 1 to the count of infected agents
                     break   #stops running the for loop
                     
         elif self.type == 1:    #this block happens if the agent is currently infected
             if self.random.random() <= self.model.gamma:    #generates a random number and checks if it is less than "gamma" (chance of recovering)
-                self.type = 4   #sets the agent to be a temporary type which will be changed to recovered
+                self.type = 4   #sets the agent to be recovered
                 self.model.infected -= 1    #removes 1 from the count of infected agents
                 self.model.recovered += 1   #adds 1 to the count of recovered agents
 
         else:   #this block happens if the agent is currently recovered
             ()  #does nothing
-            
+
     def advance(self):  #defines what happens to each agent at each step once the step is complete
         if self.type == 3:  #this block happens if the agent is currently of the temporary infected type
             self.type = 1   #converts the agent to be infected
@@ -48,26 +52,34 @@ class Disease(mesa.Model):  #defines a class called "Disease" that inherits from
         self.beta = beta    #sets the chance of being infected
         self.gamma = gamma  #sets the chance of recovering
 
-        self.schedule = mesa.time.SimultaneousActivation(self)    #activates every agent in at once each time step
+        self.schedule = mesa.time.SimultaneousActivation(self)    #activates every agent in a random order each time step
         self.grid = mesa.space.SingleGrid(width, height, torus=False)   #creates a width*height sized grid (that does not wrap around)
 
-        count = 0   #sets up a counter for how many agents have been placed
+        xi_0 = random.sample(range(0, height), i_0) #generates a random list of i_0 amount of x coordinates
+        yi_0 = random.sample(range(0, width), i_0)  #generates a random list of i_0 amount of y coordinates
+        starting_infected = []  #creates an empty list
+        for i in range(i_0):    #runs this block i_0 times
+            starting_infected.append((xi_0[i], yi_0[i]))    #appends each of the x and y coordinates to the list
+
+        for cell in starting_infected:  #for every randomly generated cell from above, it will run this block
+            agent_type = 1  #sets the agent to be infected
+            agent = DiseaseAgent(cell, self, agent_type)    #sets an agent up with its position and type
+            self.grid.place_agent(agent, cell)  #places the agent in the grid
+            self.schedule.add(agent)    #adds the agent to the scheduler
+
         for cell in self.grid.coord_iter(): #for every cell in the grid, it will run this block
             x = cell[1] #sets x equal to the x coordinate of the cell
             y = cell[2] #sets y equal to the y coordinate of the cell
-            if self.random.random() < self.density: #generates a random number, runs this block if that number is less than the density
-                if count < i_0: #runs this block if the counter is less than the amount of initial infected agents
-                    agent_type = 1  #sets the agent to be infected
-                    count += 1  #ups the count by 1
-                else:   #runs this block if the previous did not run
-                    agent_type = 0  #sets the agent to be susceptible
-
+            if (x,y) in starting_infected:  #checks if the cell is one of the generated infected 
+                pass    #does nothing if so
+            elif self.random.random() < self.density: #generates a random number, runs this block if that number is less than the density. Only checks if the previous fails
+                agent_type = 0  #sets the agent to be susceptible
                 agent = DiseaseAgent((x,y), self, agent_type)   #sets an agent up with its position and type
                 self.grid.place_agent(agent, (x,y)) #places the agent in the grid
-                self.schedule.add(agent)    #adds the agent to the scheduler
+                self.schedule.add(agent)    #adds the agent to the scheduler            
 
-        self.susceptible = self.schedule.get_agent_count()-i_0    #sets the count of susceptible agents equal to the total count minus the starting infected amount
-        self.infected = i_0   #sets the count of infected agents equal to the starting number of infected agents
+        self.susceptible = self.schedule.get_agent_count()-i_0    #sets the count of susceptible agents equal to the total count minus 1
+        self.infected = i_0   #sets the count of infected agents equal to the s
         self.recovered = 0  #sets the count of recovered agents equal to 0
         self.datacollector = mesa.DataCollector(
             {"susceptible": "susceptible", "infected": "infected", "recovered": "recovered"},
@@ -84,5 +96,3 @@ class Disease(mesa.Model):  #defines a class called "Disease" that inherits from
 
         if self.infected == 0:  #if the amount of infected agents is 0, runs this
             self.running = False    #stops the code from running
-
-        
