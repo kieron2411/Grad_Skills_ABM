0171d81c67e2b80e512d2a916e698b30c63dc650
kieron2411 <59542777+kieron2411@users.noreply.github.com>
2022-12-16 12:36:00 +0000

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,3 +1,5 @@
+#Credit goes to Project Mesa whose code this is based off of: https://github.com/projectmesa/mesa-examples/tree/main/examples/Schelling
+
 from Server import server
 
 server.launch()
